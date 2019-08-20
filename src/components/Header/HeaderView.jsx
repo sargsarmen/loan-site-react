ede4dc1067fb57c80f2ab7c8d7cb43c142facb2e
Armen Sargsyan <armen.sargsyan@webbfontain.com>
2019-08-21 02:37:15 +0400

    Header toggle

--- src/components/Header/HeaderView.jsx
+++ src/components/Header/HeaderView.jsx
@@ -1,39 +1,66 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback, useState } from "react";
 import { Link } from "react-router-dom";
+import className from "classnames";
 import * as pathnames from "../../constants/pathnames";
 
-import headerStyles from "./Header.module.css";
+import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.png";
 
+const cx = className.bind(styles);
+
 const HeaderView = () => {
+  const [visible, setVisible] = useState(true);
+  const menuEl = useRef(null);
+  const onClick = useCallback(() => {
+    setVisible(!visible);
+  }, [visible]);
+
+  useEffect(() => {
+    menuEl.current.addEventListener("click", onClick);
+
+    return () => {
+      menuEl.current.removeEventListener("click", onClick);
+    };
+  }, []);
+
   return (
-    <header className={headerStyles.header}>
-      <div className={headerStyles.wrapper}>
-        <Link to={pathnames.home} className={headerStyles.logo}>
+    <header className={styles.header}>
+      <div className={styles.wrapper}>
+        <Link to={pathnames.home} className={styles.logo}>
           <img src={logo} alt="logo" />
         </Link>
-        <div className={headerStyles.menuWrap}>
-          <div className={headerStyles.menu}>
-            <div className={headerStyles.menuItem}>
-              <Link to={pathnames.privacyPolicy} className={headerStyles.menuLink}>
+        <button type="button" className={cx([styles.toggle], { [styles.active]: visible })} onClick={onClick}>
+          <s className={styles.bar} />
+          <s className={styles.bar} />
+          <s className={styles.bar} />
+        </button>
+        <div ref={menuEl} className={cx([styles.menuWrap], { [styles.show]: visible })}>
+          <div className={styles.menu}>
+            <div className={styles.menuItem}>
+              <Link to={pathnames.privacyPolicy} className={styles.menuLink}>
                 HOW IT WORKS
               </Link>
             </div>
 
-            <div className={headerStyles.menuItem}>
-              <Link to={pathnames.privacyPolicy} className={headerStyles.menuLink}>
+            <div className={styles.menuItem}>
+              <Link to={pathnames.privacyPolicy} className={styles.menuLink}>
                 ABOUT US
               </Link>
             </div>
 
-            <div className={headerStyles.menuItem}>
-              <Link to={pathnames.ratesAndFees} className={headerStyles.menuLink}>
+            <div className={styles.menuItem}>
+              <Link to={pathnames.ratesAndFees} className={styles.menuLink}>
                 RATES AND FEES
               </Link>
             </div>
+            <div className={cx([styles.menuItem], [styles.hidden])}>
+              <button type="button" className={styles.button}>
+                GET STARTED
+              </button>
+            </div>
           </div>
-          <a className={headerStyles.getStarted} href="/">
-            <button type="button" className={headerStyles.button}>
+          <a className={styles.getStarted} href="/">
+            <button type="button" className={styles.button}>
               GET STARTED
             </button>
           </a>
